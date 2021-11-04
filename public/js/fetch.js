58da517406648b57da93634606fc3d7a81703a27
Mangesh <Mangeshsang@gmail.com>
2021-11-04 19:22:40 +0530

    styling weather page

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,8 +1,9 @@
 
 
 const successData=document.getElementById('success-data');
-const errorData=document.getElementById('error-data');
 const button=document.getElementById('button');
+const HTMLtext=document.querySelector('.data-textnode');
+
 
 button.addEventListener('click',(e)=>{
     e.preventDefault()
@@ -13,19 +14,25 @@ button.addEventListener('click',(e)=>{
 .then(response=>{response.json().then((data)=>{
     if(data.error){
         console.log(data.error);
-        const para=document.createElement('p');
-        const textNode=document.createTextNode(data.error);
-        const message=para.appendChild(textNode)
-        errorData.appendChild(message)
+        textnode=data.error;
+        HTMLtext.innerHTML=textnode;
+        // const para=document.createElement('p');
+        // const textNode=document.createTextNode(data.error);
+        // const message=para.appendChild(textNode)
+        // errorData.appendChild(message)
     }else{
     console.log(data.geodata,data.weatherdata)
-    const para=document.createElement('p');
-    const textNode=document.createTextNode(`In ${data.geodata.location} the weather is ${data.weatherdata.weather[0]}.<br>
+    
+    const textnode=`In ${data.geodata.location} the weather is ${data.weatherdata.weather[0]}.
      ${data.weatherdata.temperature} It feels like ${data.weatherdata.feelsLike} The chances of rain today is 
-     ${data.weatherdata.chances_of_rain}. Humidity is ${data.weatherdata.humidity}`);
-    const message=para.appendChild(textNode)
-    successData.appendChild(message)
-    successData.classList.add('success-data')
+     ${data.weatherdata.chances_of_rain}. Humidity is ${data.weatherdata.humidity}`;
+
+     console.log(textnode);
+     HTMLtext.innerHTML=textnode;
+        // const para=document.createElement('p');
+    // const message=para.appendChild(textNode)
+    // successData.appendChild(message)
+    // successData.classList.add('success-data')
     }
 })})
 
