ca9d52f5b3eb3717eb874813449a682a1d53e02d
Mangesh <Mangeshsang@gmail.com>
2021-11-04 20:24:06 +0530

    added <br> in text

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -23,12 +23,16 @@ button.addEventListener('click',(e)=>{
     }else{
     console.log(data.geodata,data.weatherdata)
     
-    const textnode=`In ${data.geodata.location} the weather is ${data.weatherdata.weather[0]}.
-     ${data.weatherdata.temperature} It feels like ${data.weatherdata.feelsLike} The chances of rain today is 
-     ${data.weatherdata.chances_of_rain}. Humidity is ${data.weatherdata.humidity}`;
+    const textnode=`In ${data.geodata.location}. Latitude= ${data.geodata.latitude} , Longitude= ${data.geodata.longitude}<br>
+     The weather is ${data.weatherdata.weather[0]}.<br>
+     ${data.weatherdata.temperature}<br> 
+     It feels like ${data.weatherdata.feelsLike}<br>
+     The chances of rain today is ${data.weatherdata.chances_of_rain}.<br>
+     Humidity is ${data.weatherdata.humidity}`;
 
      console.log(textnode);
      HTMLtext.innerHTML=textnode;
+    inputText.value=""
         // const para=document.createElement('p');
     // const message=para.appendChild(textNode)
     // successData.appendChild(message)
